39edf31697f2e3161a4624eee036ae286605f583
houcheng <hczshd@163.com>
2020-12-17 11:11:32 +0800

    commit：commit util

---
+++ util/TreeNode.java
@@ -0,0 +1,15 @@
+package com.hou.util;
+
+/**
+ * 为了方便，把牛客网上的二叉树数据结构拿出来，再次使用在类中导包即可
+ */
+public class TreeNode {
+    public int val = 0;
+    public TreeNode left = null;
+    public TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+}
