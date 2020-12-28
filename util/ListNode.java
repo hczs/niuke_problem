7b0938b632ae34c146050fc4cd6cef6b22a51383
houcheng <hczshd@163.com>
2020-12-28 14:06:43 +0800

    更新util包，添加单链表（ListNode）数据结构

---
+++ util/ListNode.java
@@ -0,0 +1,14 @@
+package com.hou.util;
+
+/**
+ * 普通单链表
+ */
+
+public class ListNode {
+    public int val;
+    public ListNode next = null;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
