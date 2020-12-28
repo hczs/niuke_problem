e2143ebf4f37501ce12d4a8f51462abfc678e53a
houcheng <hczshd@163.com>
2020-12-28 14:05:32 +0800

    添加困难题：第三题，从尾到头打印链表（栈和递归都写了一遍）

--- offer/HardJZ3.java
+++ offer/HardJZ3.java
@@ -1,5 +1,9 @@
+package com.hou.offer;
 
+import com.hou.util.ListNode;
 import java.util.ArrayList;
+import java.util.Stack;
+
 public class HardJZ3{
     /**
      * 从尾到头打印链表
@@ -16,4 +20,22 @@ public class HardJZ3{
         }
         return arrayList;
     }
+
+    /**
+     * 再用栈写一下吧
+     * 循环两遍，第一次放入栈，第二次出栈返回
+     */
+    public ArrayList<Integer> printListFromTailToHead1(ListNode listNode) {
+        Stack<Integer> stack = new Stack<>();
+        while (listNode != null) {
+            stack.push(listNode.val);
+            listNode = listNode.next;
+        }
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        while (!stack.isEmpty()) {
+            res.add(stack.pop());
+        }
+        return res;
+    }
+
 }
