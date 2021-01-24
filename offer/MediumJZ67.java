714507a6cdc481ecad4e4eae2b1852df14435a05
houcheng <hczshd@163.com>
2021-01-24 13:48:34 +0800

    添加中等题：第67题，剪绳子（递归、动态规划）

--- offer/MediumJZ67.java
+++ offer/MediumJZ67.java
@@ -15,62 +15,49 @@ public class MediumJZ67 {
      * 暴力递归
      * 不想分成多少段，就只想返回的是最大值就行
      * 函数作用：返回分成m段之后的最大乘积
-     * 结束条件：当绳子长度n<=4的时候，返回target
-     * 下一步递归：我们依次尝试分成 1，n-1之后，2，n-2之后，。。。取最大值
+     * 结束条件：当绳子长度n<=3的时候，返回target-1
+     * 下一步递归：我们剪一刀之后，面临的选择有 不再继续往下剪了，还有就是继续往下剪
+     * 不再继续往下剪的时候 乘积就是 (假设在j处剪开) j*(n-j)
+     * 继续往下剪就是  j*cutRope(n-j) 求两者最大值 ----------这是一次剪开
+     * 剪n次就是  多个 max( j*(n-j), j*cutRope(n-j) )
      * 递归超时
      */
     public int cutRope(int target) {
-        if (target <= 4) {
-            return target;
+        if (target <= 3) {
+            return target-1;
         }
         int max = Integer.MIN_VALUE;
         for (int i=1; i<=target; i++) {
-            int cur = i * cutRope(target-i);
-            if (cur > max) {
-                max = cur;
-            }
+            // 要比较三个值，max 和 切一下分两段 再比较 切一下*剩下的继续分段的最大值
+            max = Math.max(max, Math.max(i*(target-i), i * cutRope(target-i)));
         }
         return max;
     }
 
     /**
      * 动态规划
-     * 自顶向下分析
-     * 设 f(n) 是长度n分段后的最大乘积
-     * 要求f(n)，就得求 max( 1*f(n-1), 2*f(n-2), 3*f(n-3), ... ,n-1*f(1) )
-     * 自底向上求解
-     * f(1) = 1     // 1不再范围之内，不过也需要1
-     * f(2) = 2
-     * f(3) = 3
-     * f(4) = 4
-     * f(5) = max(1*f(4), 2*f(3), 3*f(2), 4*f(1)) = 6
-     *
-     * f(7) = max(1*f(6), 2*f(5), 3*f(4), 4*f(3), 5*f(2), 6*f(1))
+     * 有了递归动态规划就好知道了
+     * 其实就是把函数 定义为了数组存储，分别还是 函数设计、结束条件、下一步递归
+     * 只不过递归是从上往下算
+     * 动态规划是从下往上算
+     * 定义 dp：就是存储对应长度的乘积最大值
+     * 初始状态 dp[2] = 1
+     * 转移方程：dp[i] = max(dp[i], max(j*[i-j], j*dp[i-j]))   j=1;j<i 从切一个长度为1的段开始，切1，切2...
+     * 返回 dp[n]
      */
 
     public int cutRope1(int target) {
-        if (target <= 4) {
-            return target;
+        if (target <= 3) {
+            return target-1;
         }
         int[] dp = new int[target+1];
-        for (int i=0; i<=4; i++) {
-            dp[i] = i;
-        }
-        for (int i=5; i<=target; i++) {
-            int max = Integer.MIN_VALUE;
+        dp[2] = 1;
+        for (int i=3; i<=target; i++) {
             for (int j=1; j<i; j++) {
-                max = Integer.max(max, j*dp[i-j]);
+                dp[i] = Math.max( dp[i], Math.max( j*(i-j), j*dp[i-j]) );
             }
-            dp[i] = max;
         }
         return dp[target];
     }
 
-    public static void main(String[] args) {
-        MediumJZ67 mediumJZ67 = new MediumJZ67();
-        System.out.println(mediumJZ67.cutRope(7));
-        System.out.println(mediumJZ67.cutRope(6));
-        System.out.println(mediumJZ67.cutRope1(6));
-        System.out.println(mediumJZ67.cutRope1(7));
-    }
 }
