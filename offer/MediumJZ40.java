e0cbec5274a375194d3c6d8e0a62aa496f88a0e1
houcheng <hczshd@163.com>
2021-01-26 08:40:26 +0800

    修改中等题：第40题，数组中只出现一次的数字（哈希、分组异或）

--- offer/MediumJZ40.java
+++ offer/MediumJZ40.java
@@ -15,9 +15,10 @@ public class MediumJZ40 {
      * num1,num2分别为长度为1的数组。传出参数
      * 将num1[0],num2[0]设置为返回结果
      * 用哈希表存储元素和次数，然后取出分别放入num1和num2中
+     * 13ms
      */
 
-    public void FindNumsAppearOnce(int [] array,int num1[] , int num2[]) {
+    public void FindNumsAppearOnce(int [] array, int[] num1, int[] num2) {
         HashMap<Integer, Integer> hashMap = new HashMap<>();
         for (int element : array) {
             if (hashMap.containsKey(element)) {
@@ -34,6 +35,39 @@ public class MediumJZ40 {
             }
             if (hashMap.get(element) == 1 && flag) {
                 num2[0] = element;
+            }
+        }
+    }
+
+    /**
+     * 优化
+     * 使用异或，因为异或特性是相同为0
+     * 整体异或完成之后 剩下的值 就是这两个不同的数异或的值
+     * 然后找这两个数的不同点，比如 0010 ^ 1010 = 1000
+     * 看这个结果中的从低到高第一个1，以这个为标记分组异或，再次扫描array，分出这两个值
+     * （为什么要以这个为标记分组异或，因为这样可以分别出来这两个值，为什么不用0？因为异或 两个值相同部分是0）
+     */
+
+    public void FindNumsAppearOnce1(int [] array, int[] num1, int[] num2) {
+        // 为什么可以把他初始值设置为0，因为0异或任何数都是任何数本身
+        int value = 0;
+        for (int num : array) {
+            value ^= num;
+        }
+        // 开始找标记，找不到mask就一直往左移持续寻找第一个1
+        int mask = 1;
+        while ((mask & value) == 0) {
+            mask <<= 1;
+        }
+        num1[0] = 0;
+        num2[0] = 0;
+        // 以mask为基准分组异或   0010 ^ 1010 = 1000 以这个来辅助想象， 这样分的两组只有一个不同的数，而相同的数会被异或消除掉，所以只剩下那个不同的数了
+        for (int num : array) {
+            // 注意此处分组的判断，与mask做与运算，可以把这两个数分开
+            if ((mask & num) != 0) {
+                num1[0] ^= num;
+            } else {
+                num2[0] ^= num;
             }
         }
     }
