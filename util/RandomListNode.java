692862cac1b2aac3140d116622da19852a565151
houcheng <hczshd@163.com>
2021-01-31 21:33:49 +0800

    添加复杂链表

---
+++ util/RandomListNode.java
@@ -0,0 +1,14 @@
+package com.hou.util;
+
+/**
+ * 复杂链表
+ */
+public class RandomListNode {
+    public int label;
+    public RandomListNode next = null;
+    public RandomListNode random = null;
+
+    public RandomListNode(int label) {
+        this.label = label;
+    }
+}
